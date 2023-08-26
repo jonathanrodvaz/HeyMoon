5c122a1e9b7fbc8c808b9952749f269dc496e5c1
jonathanrodvaz <jonathanrodvazquez@gmail.com>
2023-08-26 20:53:51 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        String lullaby = "Hey moon, it's just you and me tonight\n" +
+                "Everyone else is asleep\n" +
+                "Hey moon, if I was to fall\n" +
+                "I won't fall so deep\n" +
+                "Though I doubt I'm gonna\n" +
+                "You can wake me up if you wanna\n" +
+                "I would hate for you to hang there all alone\n" +
+                "The whole night through";
+
+        System.out.println(lullaby);
+    }
+}
